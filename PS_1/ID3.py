eb6656f459e3974554831971e7e1446190b2f05d
Siddharth Bhola <siddharthbhola123@yahoo.co.in>
2018-10-10 17:27:32 -0500

    update ID3 implementation and evaluation function with test

--- PS_1/ID3.py
+++ PS_1/ID3.py
@@ -1,5 +1,6 @@
 from node import Node
 import math
+CLASS = "Class"
 
 def ID3(examples, default):
   '''
@@ -8,92 +9,165 @@ def ID3(examples, default):
   and the target class variable is a special attribute with the name "Class".
   Any missing attributes are denoted with a value of "?"
   '''
-  id3Tree = Node(None, 'root', None)
-  columnArray = list(examples[0].keys())
-  columnArray.remove('Class')
-  createTree(examples, columnArray, id3Tree)
-  return id3Tree
+  root = Node(None, None, None)
+  attributes = getAttributesList(examples)
+  buildTree(examples, default, root, attributes)
+  return root
 
+def buildTree(examples, default, parentNode, attributes):
+  if not examples:
+    parentNode.addChild(Node(default, default, default))
+  elif isNonTrivialSplitPossible(examples) == False:
+    modeClass = getModeClassLabel(examples)
+    parentNode.addChild(Node(modeClass, modeClass, modeClass))
+  else:
+    bestAttribute = getBestAttribute(examples, attributes)
+    possibleValues = getPossibleValuesForAttribute(examples, bestAttribute)
+    for value in possibleValues:
+      child = Node(bestAttribute, value, None)
+      examplesWithBesAttributeValue = getExamplesWithBestAttributeValue(examples, bestAttribute, value)
+      modeOfExampleWithBestValue = getModeClassLabel(examplesWithBesAttributeValue)
+      if attributes.__contains__(bestAttribute):
+        attributes.remove(bestAttribute)
+      buildTree(examplesWithBesAttributeValue, modeOfExampleWithBestValue, child, attributes)
+      parentNode.addChild(child)
+
+def getExamplesWithBestAttributeValue(examples, bestAttribute, value):
+  examplesWithBestValue = []
+  for example in examples:
+    if example[bestAttribute] is value:
+      examplesWithBestValue.append(example)
+  return examplesWithBestValue
+
+def isNonTrivialSplitPossible(examples):
+  firstClassification = examples[0][CLASS]
+  for example in examples:
+    if(example[CLASS] != firstClassification):
+      return True
+  return False
+
+def getModeClassLabel(examples):
+  classCountMap = dict()
+  for example in examples:
+    if example[CLASS] in classCountMap:
+      classCountMap[example[CLASS]] += 1
+    else:
+       classCountMap[example[CLASS]] = 1
+  maxOccuringClassLabel = None
+  maxOccuringClassCount = None
+  for key, value in classCountMap.items():
+    if maxOccuringClassLabel is None:
+      maxOccuringClassLabel = key
+      maxOccuringClassCount = value
+    elif value > maxOccuringClassCount:
+      maxOccuringClassCount = value
+      maxOccuringClassLabel = key
+  return maxOccuringClassLabel
+
+def getAttributesList(examples):
+  keys = list(examples[0].keys())
+  keys.remove(CLASS)
+  return keys
+  
+def getPossibleValuesForAttribute(examples, attribute):
+  values = set()
+  for example in examples:
+    values.add(example[attribute])
+  return list(values)
+
+def getBestAttribute(examples, attributes):
+  '''
+  calculate the Entropy for each attribute
+  select the one which has the least entropy
+  '''  
+  bestAttribute = None
+  minEntropy = None
+  for attribute in attributes:
+    entropy = getInfoGainForAttribute(examples, attribute)
+    if entropy is None:
+      continue    
+    elif minEntropy is None:
+      minEntropy = entropy
+      bestAttribute = attribute
+    elif entropy < minEntropy:
+      minEntropy = entropy
+      bestAttribute = attribute
+  return bestAttribute
+
+def getInfoGainForAttribute(examples, attribute):
+  '''
+  1. list the possible output values for this attribute
+  2. foreach attribute value: calculate P(Ai = v) * entropy of Y for Ai = v
+  3. return sum of above 
+  '''
+  attrOutputValues = set()  # unique output values possible for this attribute
+  # attributeValueOutputListMap = []
+  for example in examples:
+    attrOutputValues.add(example[attribute])
+    # attributeValueOutputListMap.append(dict(example[attribute], example["class"]))
+  if len(attrOutputValues) is 1:
+    return None
+  totalEntropy = 0
+  examplesCount = len(examples)
+
+  for attrValue in attrOutputValues:
+    attrValueCount = 0
+    possibleClassValues = set()    
+    classCountMap = dict()
+    for example in examples:
+      if(example[attribute] == attrValue):
+        attrValueCount += 1
+        possibleClassValues.add(example[CLASS])
+        if(classCountMap.__contains__(example[CLASS])):
+          classCountMap[example[CLASS]] +=1
+        else: classCountMap[example[CLASS]] = 1
+    attrValueProbability = attrValueCount / examplesCount
+    classEntropyForAttrValue = 0
+    classLength = len(classCountMap)
+    for classValue in possibleClassValues:
+      classCount = classCountMap[classValue]
+      classValueProbability = classCount / classLength
+      classEntropyForAttrValue += (classValueProbability) * (math.log2(classValueProbability))
+  totalEntropy += attrValueProbability * classEntropyForAttrValue
+  return totalEntropy
 
 def prune(node, examples):
   '''
   Takes in a trained tree and a validation set of examples.  Prunes nodes in order
   to improve accuracy on the validation data; the precise pruning strategy is up to you.
   '''
+  originalAccuracy = test(node, examples)
+  
+
 
 def test(node, examples):
   '''
   Takes in a trained tree and a test set of examples.  Returns the accuracy (fraction
   of examples the tree classifies correctly).
   '''
-
+  totalExamples = len(examples)
+  correctlyClassifiedExamplesCount = 0
+  for example in examples:
+    result = evaluate(node, example)
+    if result == example[CLASS]:
+      correctlyClassifiedExamplesCount += 1
+  return correctlyClassifiedExamplesCount / totalExamples
 
 def evaluate(node, example):
   '''
   Takes in a tree and one example.  Returns the Class value that the tree
   assigns to the example.
-  '''
+  ''' 
+  tempNode = node
+  while tempNode.children is not None:
+    for child in tempNode.children:
+      if(child.output is not None):
+        return child.output
+      elif example[child.attribute] == child.value:
+        tempNode = child
+        continue
+  return None
 
-def findBestAttribute(data, availableLabels):
-  attribMap = dict()
-  bestAttribName = ''
-  bestAttribValue = 1
-  for label in availableLabels:
-    attribMap[label] = dict()
 
-  for row in data:
-    for key in row:
-      if key != 'Class' and key in availableLabels:
-        if not '_total' in attribMap[key]:
-          attribMap[key]['_total'] = 0
-        attribMap[key]['_total'] += 1
-        if not str(row[key]) in attribMap[key]:
-          attribMap[key][str(row[key])] = dict()
-          attribMap[key][str(row[key])]['_total'] = 0
-        if not str(row['Class']) in attribMap[key][str(row[key])]:
-          attribMap[key][str(row[key])][str(row['Class'])] = 0
-        attribMap[key][str(row[key])][str(row['Class'])] += 1
-        attribMap[key][str(row[key])]['_total'] += 1
 
-  for attrib in attribMap:
-    attribGain = 0
-    attribTotal = attribMap[attrib]['_total']
-    for key in attribMap[attrib]:
-      if key != '_total':
-        keyTotal = attribMap[attrib][key]['_total']
-        for subKey in attribMap[attrib][key]:
-          if subKey != '_total':
-            attribGain += ((1.0 * keyTotal) / attribTotal) * ((-1) * ( ((1.0 * attribMap[attrib][key][subKey]) / keyTotal) * math.log(((1.0 * attribMap[attrib][key][subKey]) / keyTotal)) ) )
-
-    attribMap[attrib]['_ig'] = attribGain
-    if bestAttribValue > attribGain:
-      bestAttribValue = attribGain
-      bestAttribName = attrib
-
-  returnMap = dict()
-  returnMap[bestAttribName] = attribMap[bestAttribName]
-  return returnMap
-
-def createTree(data, availableAttributes, treeNode):
-  bestAttributeInfo = findBestAttribute(data, availableAttributes)
-  bestAttrib = next(iter(bestAttributeInfo))
-  bestAttributeInfo = bestAttributeInfo[bestAttrib]
-  del bestAttributeInfo['_total']
-  del bestAttributeInfo['_ig']
-
-  for value in bestAttributeInfo:
-    tempChild = Node(value, bestAttrib, None)
-    if len(bestAttributeInfo[value].keys()) == 2:
-      output = list(bestAttributeInfo[value].keys())
-      output.remove('_total')
-      tempChild.output = output[0]
-      treeNode.children.append(tempChild)
-    else:
-      subData = []
-      for instance in data:
-        if str(instance[bestAttrib]) == str(value):
-          subData.append(instance)
-      treeNode.children.append(tempChild)
-      tempAvailableAttributes = availableAttributes.copy()
-      tempAvailableAttributes.remove(bestAttrib)
-      createTree(subData, tempAvailableAttributes, tempChild)
+  
